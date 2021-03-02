d07316fda7a7e956c0de145096f53bc6164785d8
Kaesid <keskaesid@gmail.com>
2021-03-02 12:47:00 +0400

    feat: add function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+module.exports = function check(str, bC) {
+    // console.log(str);
+    if (
+        typeof str !== "string" ||
+        str.trim().length === 0 ||
+        str.trim().length % 2 !== 0
+    ) {
+        return false;
+    }
+    let arr = [];
+    for (let char of str) {
+        for (let i = 0; i < bC.length; i++) {
+            const eq = bC[i][0] === bC[i][1];
+
+            if (
+                char === bC[i][0] &&
+                (!eq || arr.length === 0 || arr[arr.length - 1] != bC[i][0])
+            ) {
+                // console.log("ADD", char);
+                arr.push(char);
+                // console.log(arr);
+            } else if (char === bC[i][1]) {
+                if (arr.length === 0 || arr[arr.length - 1] !== bC[i][0]) {
+                    return false;
+                } else {
+                    // console.log("REMOVE", char);
+                    arr.pop();
+                    // console.log(arr);
+                }
+            }
+        }
+    }
+    if (arr.length !== 0) {
+        // console.log(arr);
+        return false;
+    }
+
+    return true;
+};
